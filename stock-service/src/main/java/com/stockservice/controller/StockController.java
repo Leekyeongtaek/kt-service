b1f8777a510db197a3ee0a707b186443f5e1f3c9
mrlee <lkt900520@gmail.com>
2026-03-29 12:56:56 +0900

    feat: 선착순 매수 재고 설정 및 매수 신청 API 추가

--- stock-service/src/main/java/com/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/stockservice/controller/StockController.java
@@ -1,30 +1,27 @@
 package com.stockservice.controller;
 
-import com.stockservice.domain.Stock;
 import com.stockservice.dto.StockDetailResponse;
+import com.stockservice.dto.request.StockLimitedOfferPurchaseRequest;
 import com.stockservice.dto.StockRankingResponse;
 import com.stockservice.dto.StockResponse;
+import com.stockservice.enums.LimitedOfferResult;
 import com.stockservice.service.StockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/stocks")
+@RequestMapping("/api/v1/stocks")
 @RestController
 public class StockController {
 
     private final StockService stockService;
 
-    //todo 반환 데이터 축약
     @GetMapping("/search")
     public ResponseEntity<Page<StockResponse>> getStocks(Pageable pageable) {
         Page<StockResponse> stockResponses = stockService.searchStock(pageable);
@@ -39,6 +36,25 @@ public class StockController {
     @GetMapping("/rankings")
     public ResponseEntity<List<StockRankingResponse>> getTop10Rankings() {
         return ResponseEntity.ok(stockService.getTop10Rankings());
+    }
+
+    //limited-offers: 한정 상품
+    @PostMapping("/{stockId}/limited-offers")
+    public ResponseEntity<String> createLimitedOffer(
+            @PathVariable(value = "stockId") Long stockId,
+            @RequestParam int quantity) {
+
+        stockService.createLimitedOffer(stockId, quantity);
+        return ResponseEntity.ok("종목 ID " + stockId + "의 한정 상품 재고가 " + quantity + "개로 설정되었습니다.");
+    }
+
+    @PostMapping("/{stockId}/limited-offers/purchases")
+    public ResponseEntity<String> applyStockLimitedOffer(
+            @PathVariable Long stockId,
+            @RequestBody StockLimitedOfferPurchaseRequest request) {
+
+        LimitedOfferResult limitedOfferResult = stockService.applyLimitedOffer(stockId, request);
+        return ResponseEntity.ok(limitedOfferResult.getMessage());
     }
 
     @GetMapping("/version")
