96acecefad0fbadd5db77cdd9b3ec2ddf103b0e8
mrlee <lkt900520@gmail.com>
2026-03-31 21:27:26 +0900

    chore: RedisConfig 주석

--- stock-service/src/main/java/com/stockservice/common/config/RedisConfig.java
+++ stock-service/src/main/java/com/stockservice/common/config/RedisConfig.java
@@ -23,25 +23,30 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    //Jedis (전통적인 강자), 동기(Blocking) 방식
-    //스레드 안전하지 않음 (풀링 필요), 상대적으로 무거움 (멀티스레드 시)
-    //application.yml에 설정한 값을 자동으로 읽어와 연결
+    // Jedis: Lettuce 이전에 쓰이던 클래스
+    // Thread-sage 하지 않아서 별도의 쓰레드 풀을 만들어야 하고 느리고 무겁다.
 //    @Bean
 //    public JedisConnectionFactory jedisConnectionFactory() {
 //        return new JedisConnectionFactory();
 //    }
 
-    //Lettuce (신흥 강자, 스프링 기본값), 비동기(Non-blocking) / 리액티브 방식
-    //스레드 안전함 (커넥션 공유 가능), 효율적이고 빠름 (Netty 기반)
-    //application.yml에 설정한 값을 자동으로 읽어와 연결
+    // RedisConnectionFactory: 스프링과 레디스 서버 사이에 통신 다리
+    // 스프링 부트 2.0 이후 기본 설정 클래스
+    // 비동기 처리(Non-blocking), 성능 최적화
+    // 스레드 안전함 (커넥션 공유 가능), 효율적이고 빠름 (Netty 기반)
+    // application.yml에 설정한 값을 자동으로 읽어와 연결
     @Primary
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
-    //spring-boot-starter-data-redis 라이브러리는 스프링 부트의 자동 설정 대상: 포트, 호스트 정보 입력 안한 이유
-    //데이터를 저장하고 꺼내오는 캐싱/랭킹 용도(Lettuce 기반)로 사용
+    // Redisson: 단순 캐싱이 아닌 분산 락 기능이 필요한 경우 사용
+
+    // spring-boot-starter-data-redis 라이브러리는 스프링 부트의 자동 설정 대상: 포트, 호스트 정보 입력 안한 이유
+    // 레디스 서버에 CRUD 기능을 모두 제공하는 만능 도구
+    // Key, Value 모두 String만 사용한다면 StringRedisTemplate 사용 가능
+    //todo 추후 직렬화 방식 변경 테스트 필요.
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
@@ -55,6 +60,7 @@ public class RedisConfig {
         return redisTemplate;
     }
 
+    // 선착순 구매 레디스 루아 스크립트를 빈으로 등록해 재사용
     @Bean
     public RedisScript<Long> purchaseLimitedOfferScript() {
         DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
