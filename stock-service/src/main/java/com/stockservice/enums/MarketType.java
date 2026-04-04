7a329ae455903bd577771c8016b5069852902947
mrlee <lkt900520@gmail.com>
2026-04-04 18:18:54 +0900

    feat: MarketType KONEX, StockType CERTIFICATE_OF_CLASS_SHARE 속성 추가

--- stock-service/src/main/java/com/stockservice/enums/MarketType.java
+++ stock-service/src/main/java/com/stockservice/enums/MarketType.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum MarketType {
     KOSPI("코스피"),
     KOSDAQ("코스닥"),
-    KOSDAQ_GLOBAL("코스닥 글로벌");
+    KOSDAQ_GLOBAL("코스닥 글로벌"),
+    KONEX("코넥스");
 
     private final String marketTypeName;
 }
--- stock-service/src/main/java/com/stockservice/enums/StockType.java
+++ stock-service/src/main/java/com/stockservice/enums/StockType.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum StockType {
     COMMON("보통주"),
     OLD_PREFERRED("구형우선주"),
-    NEW_PREFERRED("신형우선주");
+    NEW_PREFERRED("신형우선주"),
+    CERTIFICATE_OF_CLASS_SHARE("종류주권");
 
     private final String StockTypeName;
 }
